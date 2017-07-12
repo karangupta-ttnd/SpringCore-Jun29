e031e277de4bdcaaad70c684abfea72c6323ec05
karan <karangupta.dev@gmail.com>
2017-07-12 16:59:06 +0530

    Files added

---
+++ src/Database.java
@@ -0,0 +1,20 @@
+/**
+ * Created by karan on 12/7/17.
+ */
+public class Database {
+
+
+    int port;
+    String name;
+
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}

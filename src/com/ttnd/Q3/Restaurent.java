e5e4cfd6578c11e22163a08da1219aa5b440f03c
karan <karangupta.dev@gmail.com>
2017-07-14 01:11:24 +0530

    Q8- added @Required annotation on setter method

--- src/com/ttnd/Q3/Restaurent.java
+++ src/com/ttnd/Q3/Restaurent.java
@@ -1,5 +1,7 @@
 package com.ttnd.Q3;
 
+import org.springframework.beans.factory.annotation.Required;
+
 /**
  * Created by karan on 13/7/17.
  */
@@ -7,11 +9,13 @@ public class Restaurent {
 
     HotDrink hotDrink1;
 
-    public Restaurent(HotDrink hotDrink){
-        this.hotDrink1=hotDrink;
-    }
-
     public HotDrink getHotDrink1() {
         return hotDrink1;
     }
+
+    @Required
+    public void setHotDrink1(HotDrink hotDrink) {
+        this.hotDrink1 = hotDrink;
+    }
+
 }

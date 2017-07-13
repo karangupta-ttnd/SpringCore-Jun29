dbeefedaa222375fb04c5a736b25eacef067c097
karan <karangupta.dev@gmail.com>
2017-07-14 00:26:40 +0530

    Q6-Invoked prepareHotdrink() from Tea

--- src/com/ttnd/Main.java
+++ src/com/ttnd/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Restaurent restaurent =(Restaurent)context.getBean("restaurent");
-        restaurent.getHotDrink().prepareHotDrink();
+       restaurent.getHotDrink1().prepareHotDrink();
     }
 
 }

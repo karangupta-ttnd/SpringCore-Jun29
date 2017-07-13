bcfde243f9df91f28b147daff67ee5f8009b1042
karan <karangupta.dev@gmail.com>
2017-07-14 02:28:27 +0530

    Q10-used @controller annotation

--- src/com/ttnd/Main.java
+++ src/com/ttnd/Main.java
@@ -1,22 +1,20 @@
 package com.ttnd;
 
-import com.ttnd.Q3.ExpressTea;
-import com.ttnd.Q3.HotDrink;
 import com.ttnd.Q3.Restaurent;
-import com.ttnd.Q3.Tea;
-import com.ttnd.Q5.Complex;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by karan on 13/7/17.
  */
+@Controller
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Restaurent restaurent =(Restaurent)context.getBean("teaRestaurent");
-        System.out.println(context.isPrototype("teaRestaurent"));
+        Restaurent restaurent =(Restaurent)context.getBean("restaurent");
+        restaurent.getHotDrink1().prepareHotDrink();
     }
 
 }

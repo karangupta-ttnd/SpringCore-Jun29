190bd991d676496fc6398e15232789462565b498
karan <karangupta.dev@gmail.com>
2017-07-14 02:05:48 +0530

    Q9-using Autowired annotation with a setter

--- src/com/ttnd/Q3/Restaurent.java
+++ src/com/ttnd/Q3/Restaurent.java
@@ -1,21 +1,22 @@
 package com.ttnd.Q3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
+
 
 /**
  * Created by karan on 13/7/17.
  */
 public class Restaurent {
 
-    HotDrink hotDrink1;
+    private HotDrink hotDrink1;
 
-    public HotDrink getHotDrink1() {
-        return hotDrink1;
-    }
-
-    @Required
+    @Autowired
     public void setHotDrink1(HotDrink hotDrink) {
         this.hotDrink1 = hotDrink;
     }
 
+    public HotDrink getHotDrink1() {
+        return hotDrink1;
+    }
 }

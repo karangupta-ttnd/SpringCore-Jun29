270ad535fe3908d73bdd4e57ab0e8f8af06d687c
karan <karangupta.dev@gmail.com>
2017-07-14 02:30:31 +0530

    Q10-used @component annotation

--- src/com/ttnd/Q3/Tea.java
+++ src/com/ttnd/Q3/Tea.java
@@ -1,8 +1,11 @@
 package com.ttnd.Q3;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by karan on 13/7/17.
  */
+@Component
 public class Tea implements HotDrink {
     @Override
     public void prepareHotDrink() {

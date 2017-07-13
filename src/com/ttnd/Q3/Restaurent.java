842f74c8d195e32ff38e1c0d4d4d5e8b42648824
karan <karangupta.dev@gmail.com>
2017-07-14 00:45:34 +0530

    Q6-created constructor and getter()

--- src/com/ttnd/Q3/Restaurent.java
+++ src/com/ttnd/Q3/Restaurent.java
@@ -6,21 +6,12 @@ package com.ttnd.Q3;
 public class Restaurent {
 
     HotDrink hotDrink1;
-    HotDrink hotDrink2;
+
+    public Restaurent(HotDrink hotDrink){
+        this.hotDrink1=hotDrink;
+    }
 
     public HotDrink getHotDrink1() {
         return hotDrink1;
-    }
-
-    public void setHotDrink1(HotDrink hotDrink1) {
-        this.hotDrink1 = hotDrink1;
-    }
-
-    public HotDrink getHotDrink2() {
-        return hotDrink2;
-    }
-
-    public void setHotDrink2(HotDrink hotDrink2) {
-        this.hotDrink2 = hotDrink2;
     }
 }

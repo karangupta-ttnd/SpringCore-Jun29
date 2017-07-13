1ecd09901e2600745470063abe5369d238f73482
karan <karangupta.dev@gmail.com>
2017-07-14 00:37:29 +0530

    Q6- methods invoked from hotDrink1 and hotDrink2

--- src/com/ttnd/Main.java
+++ src/com/ttnd/Main.java
@@ -16,7 +16,8 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Restaurent restaurent =(Restaurent)context.getBean("restaurent");
-       restaurent.getHotDrink1().prepareHotDrink();
+        restaurent.getHotDrink1().prepareHotDrink();
+        restaurent.getHotDrink2().prepareHotDrink();
     }
 
 }

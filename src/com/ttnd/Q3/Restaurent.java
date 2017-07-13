00c6f62530baf19e66b59a2cbfff96b7239b9bda
karan <karangupta.dev@gmail.com>
2017-07-14 02:07:35 +0530

    Q9-using Autowired annotation with constructor

--- src/com/ttnd/Q3/Restaurent.java
+++ src/com/ttnd/Q3/Restaurent.java
@@ -9,13 +9,13 @@ import org.springframework.beans.factory.annotation.Required;
  */
 public class Restaurent {
 
-    @Autowired
+
     private HotDrink hotDrink1;
 
-    public void setHotDrink1(HotDrink hotDrink) {
-        this.hotDrink1 = hotDrink;
-    }
-
+    @Autowired
+   public Restaurent(HotDrink hotDrink){
+       hotDrink1=hotDrink;
+   }
     public HotDrink getHotDrink1() {
         return hotDrink1;
     }

dedc71e1df4135708d6a7d58eb9bdbaf254eebc8
karan <karangupta.dev@gmail.com>
2017-07-14 02:29:49 +0530

    Q10-used @repository annotation

--- src/com/ttnd/Q3/Restaurent.java
+++ src/com/ttnd/Q3/Restaurent.java
@@ -1,14 +1,17 @@
 package com.ttnd.Q3;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 
 
 /**
  * Created by karan on 13/7/17.
  */
+@Repository
 public class Restaurent {
-
 
     private HotDrink hotDrink1;
 
@@ -18,5 +21,9 @@ public class Restaurent {
    }
     public HotDrink getHotDrink1() {
         return hotDrink1;
+    }
+    public void setHotDrink1(HotDrink hotDrink)
+    {
+        hotDrink1=hotDrink;
     }
 }

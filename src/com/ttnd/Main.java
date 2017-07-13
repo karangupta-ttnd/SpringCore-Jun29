c9ea6dd93d2ccf477142ca92649adf4a4d256598
karan <karangupta.dev@gmail.com>
2017-07-13 22:06:44 +0530

    Q3-Invoked prepareHotDrink() from HotDrink instance

--- src/com/ttnd/Main.java
+++ src/com/ttnd/Main.java
@@ -1,5 +1,8 @@
 package com.ttnd;
 
+import com.ttnd.Q3.ExpressTea;
+import com.ttnd.Q3.HotDrink;
+import com.ttnd.Q3.Tea;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,8 +13,15 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Database db =(Database) context.getBean("db");
-        System.out.println(db.toString());
+//        Database db =(Database) context.getBean("db");
+//        System.out.println(db.toString());
+
+        HotDrink expressTea =(ExpressTea) context.getBean("expressTea");
+        HotDrink tea =(Tea) context.getBean("tea");
+        expressTea.prepareHotDrink();
+        tea.prepareHotDrink();
+
+
     }
 
 }

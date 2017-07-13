d70dc43775482067744b8d1ca7c423d40418d022
karan <karangupta.dev@gmail.com>
2017-07-13 21:32:39 +0530

    Created a class Database with setter methods

--- src/com/ttnd/Database.java
+++ src/com/ttnd/Database.java
@@ -1,8 +1,26 @@
+package com.ttnd;
+
 /**
  * Created by karan on 12/7/17.
  */
-public class Database  {
+public class Database {
+
     private int port;
     private String name;
 
+    @Override
+    public String toString() {
+        return "Database{" +
+                "port=" + port +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
